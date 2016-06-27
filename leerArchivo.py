de33256bbf58d51f1203c2b93487ad85146075bc
Edgardo <emoron@gmail.com>
2016-06-27 12:43:55 -0500

    update

---
+++ leerArchivo.py
@@ -0,0 +1,18 @@
+def lines(nombreArchivo,N):
+ try:
+  f = open(nombreArchivo,"r")
+  l = f.readlines()
+  return l[:N]
+ except:
+  return("error")
+
+asdklasjkdla sd
+asd
+a 
+sd
+as
+d
+as
+da
+s
+d
